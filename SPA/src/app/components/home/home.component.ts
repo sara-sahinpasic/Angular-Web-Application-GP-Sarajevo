83c30e24703357b1f1b1a1fd42f51f5b90b203a9
Amor Osmic <amor.osmic@edu.fit.ba>
2022-12-01 18:36:28 +0100

    Kreirana home komponenta

--- SPA/src/app/components/home/home.component.ts
+++ SPA/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestServiceService } from 'src/app/services/test-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private testService: TestServiceService) { }
+
+  click() {
+    this.testService.testMethod();
+  }
 }
