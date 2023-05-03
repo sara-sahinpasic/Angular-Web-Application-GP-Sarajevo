22f29e8ac6c8291e8c463927640fcad5659ad8eb
Sara Sahinpasic <sara.sahinpasic@edu.fit.ba>
2023-05-03 21:42:38 +0200

    Tasks #62 #72: Corrected mistakes

--- RestAPI/Prodaja karata za gradski prijevoz/Presentation/DTO/User/UpdateProfileDataDto.cs
+++ RestAPI/Prodaja karata za gradski prijevoz/Presentation/DTO/User/UpdateProfileDataDto.cs
@@ -9,10 +9,10 @@ namespace Presentation.DTO.User
 {
     internal class UpdateProfileDataDto
     {
+        public Guid Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
-        public Guid Id { get; set; }
     }
 }
