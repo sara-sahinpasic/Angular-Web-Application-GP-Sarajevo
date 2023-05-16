6c0e0f8389e4190279e1aeafbbc7827a65ce63c9
Sara Sahinpasic <sara.sahinpasic@edu.fit.ba>
2023-05-16 14:38:05 +0200

    Tasks: #62: Added: ToDo, #73: Spinner completed

---
+++ SPA/src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,17 @@
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { SpinnerService } from 'src/app/services/spinner/spinner.service';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class SpinnerComponent {
+  constructor(public loader: SpinnerService) {}
+}
---
+++ SPA/src/app/http-interceptors/loading.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, finalize } from 'rxjs';
+import { SpinnerService } from '../services/spinner/spinner.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  private totalRequests = 0;
+  constructor(private spinnerService: SpinnerService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    console.log('caught');
+    this.totalRequests++;
+    this.spinnerService.setLoading(true);
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.totalRequests--;
+        if (this.totalRequests == 0) {
+          this.spinnerService.setLoading(false);
+        }
+      })
+    );
+  }
+}
