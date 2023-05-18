6a5d2ae82125591abc4f0c703dff13d8ab0f76e8
Sara Sahinpasic <sara.sahinpasic@edu.fit.ba>
2023-05-18 16:22:40 +0200

    Task #77: Add Guards

---
+++ SPA/src/app/guards/logged-in.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedInGuard implements CanActivate {
+  constructor(private userService: UserService) {}
+
+  isLoggedIn: any = false;
+
+  canActivate() {
+    this.isLoggedIn = this.userService.getUser();
+
+    if (this.isLoggedIn != undefined) return true;
+    else return false;
+  }
+}
