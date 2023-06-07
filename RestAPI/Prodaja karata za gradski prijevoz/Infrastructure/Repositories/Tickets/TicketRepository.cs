0b0122e010bf5115fda906be1ddfa1f21a8d8b3a
Amor Osmic <amor.osmic@edu.fit.ba>
2023-06-07 13:34:06 +0200

    Task #107: Create Ticket entity and repository if needed

---
+++ RestAPI/Prodaja karata za gradski prijevoz/Infrastructure/Repositories/Tickets/TicketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Services.Abstractions.Interfaces.Repositories.Tickets;
+using Domain.Entities.Tickets;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories.Tickets;
+
+public sealed class TicketRepository : GenericRepository<Ticket>, ITicketRepository
+{
+    public TicketRepository(DataContext dataContext) : base(dataContext) {}
+}
