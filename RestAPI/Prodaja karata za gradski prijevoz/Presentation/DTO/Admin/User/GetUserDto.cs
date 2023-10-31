4fb6b4590e1ebec9f28a43bd8a5bdb9fd1bb74aa
Sara Šahinpašić <sara.sahinpasic@edu.fit.ba>
2023-10-31 19:08:47 +0000

    Merged PR 105: Fix for bug #238: User's saved and updated address
    Fix for bug #238: User's saved and updated address
    
    Related work items: #238

--- RestAPI/Prodaja karata za gradski prijevoz/Presentation/DTO/Admin/User/GetUserDto.cs
+++ RestAPI/Prodaja karata za gradski prijevoz/Presentation/DTO/Admin/User/GetUserDto.cs
@@ -8,5 +8,6 @@
         public string RoleName { get; set; }
         public Guid Id { get; set; }
         public string PhoneNumber { get; set; }
+        public string Address { get; set; }
     }
 }
